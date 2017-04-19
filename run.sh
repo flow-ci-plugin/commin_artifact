7d96ee41d0bcfc1407c485e4bf4f6a97512aea1e
Will <yh@fir.im>
2017-04-19 13:42:35 +0800

    判断文件夹是否存在

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@
 # ************************************************************
 echo "collect artifact start"
 
-cd ${FLOW_WORKSPACE}/build/$FLOW_PROJECT_NAME
+if [[ -d ${FLOW_WORKSPACE}/build/$FLOW_PROJECT_NAME ]]; then
+  cd ${FLOW_WORKSPACE}/build/$FLOW_PROJECT_NAME
+fi
 
 # 上传产物
 artifact_iteration $artifact_stores 0 $FLOW_ARTIFACT_URL
