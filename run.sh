ce301959b25c1d9751750fba5160c9b900f9c946
Will <yh@fir.im>
2017-04-19 12:13:45 +0800

    add changelog

--- run.sh
+++ run.sh
@@ -6,11 +6,13 @@
 #   Outputs:
 #
 # ************************************************************
+echo "collect artifact start"
 
 cd ${FLOW_WORKSPACE}/build/$FLOW_PROJECT_NAME
 
 # 上传产物
 artifact_iteration $artifact_stores 0 $FLOW_ARTIFACT_URL
 
+echo "collect artifact stop"
 
 flow_result $?
