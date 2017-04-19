01d653166919d66189c5baa8ca369c1517c11431
Will <yh@fir.im>
2017-04-19 12:07:30 +0800

    change

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@
 #
 # ************************************************************
 
+cd ${FLOW_WORKSPACE}/build/$FLOW_PROJECT_NAME
+
 # 上传产物
 artifact_iteration $artifact_stores 0 $FLOW_ARTIFACT_URL
 
